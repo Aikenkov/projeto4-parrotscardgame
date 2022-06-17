e4a30d3c2523c1753fc00483b7e20f93afb16d0f
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-06-17 12:55:48 -0300

    Inicio do projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+function vira(element) {
+    let front = element.querySelector('front-face face');
+    front.classList.add('turn')
+    let back = element.querySelector('back-face face');
+    back.classList.add('turn')
+
+
+
+
+}
