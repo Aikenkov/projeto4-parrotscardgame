80738374d5aa65f7cf05f496ac9a535c2a3525cf
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-06-17 13:18:50 -0300

    Pergunta o número de cartas até vir um correto

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let quantidade = prompt('Escolha o número de cartas (4-14)(par)')
+distribuir()
+function distribuir() {
+    while (quantidade < 4 || quantidade > 14 || quantidade % 2 !== 0) {
 
-
+        quantidade = prompt('Escolha o número de cartas (4-14)(par)')
+    }
+    distribuir()
+}
 
 function vira(element) {
     let front = element.querySelector('front-face face');
