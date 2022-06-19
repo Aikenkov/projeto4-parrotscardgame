d8083999cc009bb2e448529f2625d5ef8a541ba2
Fabio <Fabio.rod.silva.sqpr@gmail.com>
2022-06-19 16:32:35 -0300

    Adicionei a função de virar as cartas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+/* 
 let quantidade = prompt('Escolha o número de cartasSelecionadas (4-14)(par)')
 
 function escolherQuantidade() {
@@ -8,13 +8,15 @@ function escolherQuantidade() {
         quantidade = prompt('Escolha um número par de 4 a 14')
     }
 }
-escolherQuantidade()
+escolherQuantidade() */
+
+quantidade = 8;
 
 function comparador() {
     return Math.random() - 0.5;
 }
 
-const cartasImagens = ["unicorn", "fiesta", "bobross", "explody", "metal", "revertit", "triplets"];
+const cartasImagens = ["plant1", "plant2", "plant3", "plant4", "plant5", "plant6", "plant7"];
 cartasImagens.sort(comparador);
 
 let cartasSelecionadas = [];
@@ -32,12 +34,49 @@ aleatorizar()
 function distribuir() {
     for (let i = 0; i < cartasSelecionadas.length; i++) {
         let carta = `
-        <div class="carta">
-            <img src="./images/${cartasSelecionadas[i]}parrot.gif"/>
-        </div>`;
+        <div onclick="virarCarta(this)" class="carta virar" >
+                    <div class="face verso hidden">
+                        <img src="./images/${cartasSelecionadas[i]}.gif"/>
+                    </div>
+                    <div class="face frente">
+                        <img src="./images/plantFront.png">
+                    </div>
+                </div>
+        `;
 
-        document.querySelector(".game-field").innerHTML += carta;
+        document.querySelector(".campo-de-jogo").innerHTML += carta;
 
     }
 }
 distribuir()
+
+
+let frente;
+let verso;
+
+function virarCarta(elemento) {
+    let clicada = elemento.querySelector(".frente").parentNode;
+    clicada.classList.toggle("virar");
+
+    frente = elemento.querySelector(".frente");
+    verso = elemento.querySelector(".verso");
+
+    setTimeout(esconderFrente, 500);
+    setTimeout(esconderVerso, 500);
+}
+
+function esconderVerso() {
+    if (verso.classList.contains("hidden")) {
+        verso.classList.remove("hidden")
+    } else {
+        verso.classList.add("hidden")
+    }
+}
+
+function esconderFrente() {
+    if (frente.classList.contains("hidden")) {
+        frente.classList.remove("hidden")
+    } else {
+        frente.classList.add("hidden")
+    }
+}
